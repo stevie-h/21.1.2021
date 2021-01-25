bb4f1b86490e0dc7d9348d2d56681318a91d0535
Stevie H <74920914+stevie-h@users.noreply.github.com>
2021-01-25 17:22:21 +0200

    Update stevieh_hw21012021.java

--- stevieh_hw21012021.java
+++ stevieh_hw21012021.java
@@ -1,3 +1,35 @@
 // https://github.com/qa191120/21.01.2021/blob/main/hw.txt
 
 
+package com.company;
+
+public class Circle
+{
+    double radius;
+
+    public void printSize()
+    {
+        System.out.print("The size of the circle is: ");
+        System.out.println(Math.PI * Math.pow(radius, 2));
+    }
+
+  
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Circle r5_4 = new Circle();
+        Circle r8_7 = new Circle();
+
+        r5_4.radius = 5.4;
+        r8_7.radius = 8.7;
+
+        r5_4.printSize();
+        r8_7.printSize();
+
+    }
+
+}
+
+
+
